7f92388900babedc115b2d4a71dd934365202dc3
adybsampaio <adyb.sampaio@puig.es>
2024-06-21 14:40:14 +0200

    aula 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pyxintel - Lógica de Programação com Javascript
+
+### Aula 1
+
+1. Download do projeto do curso
+2. O que são algorítmos?
+3. Exibindo mensagens no navegador
+4. Variáveis
+5. Lendo valores do navegador
--- index.js
+++ index.js
@@ -3,27 +3,9 @@ alert("Olá, sou o PyxMower! Me ajude a voltar para a base.")
 let largura = prompt("Qual a largura do terreno (m) ?");
 let comprimento = prompt("Qual o comprimento do terreno (m) ?");
 
-let x1 = 0;
-let y1 = 0;
-let x2 = largura;
-let y2 = comprimento;
-let px = parseInt(Math.random() * x2 + 1);
-let py = parseInt(Math.random() * y2 + 1);
+let x0 = 0;
+let y0 = 0;
+let x1 = largura;
+let y1 = comprimento;
 
-while (px != 0 || py != 0) {
-    let movimento = prompt(`[Pos: ${px},${py}] Indique o movimento (direção, passos - ex.: n,3)`);
-    let arrMov = movimento.split(',');
-    let direcao = arrMov[0].toUpperCase();    
-    let passos = parseInt(arrMov[1]);
-    if (direcao == 'N') {
-        py = py + passos;
-    } else if (direcao == 'S') {
-        py = py - passos;
-    } else if (direcao == 'L') {
-        px = px + passos;
-    } else if (direcao == 'O') {
-        px = px - passos;
-    }
-}
-
-alert("Carregando...")
+console.log("Coordenadas: (x0 y0 x1 y1):", x0, y0, y1, y1);
