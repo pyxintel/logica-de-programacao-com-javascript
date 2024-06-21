38d0d147a671038258f7bc5913839b6d6f1203e9
adybsampaio <adyb.sampaio@puig.es>
2024-06-21 14:58:24 +0200

    atualiza index

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ alert("Olá, sou o PyxMower! Me ajude a voltar para a base.")
 let largura = prompt("Qual a largura do terreno (m) ?");
 let comprimento = prompt("Qual o comprimento do terreno (m) ?");
 
-let x1 = 0;
-let y1 = 0;
-let x2 = largura;
-let y2 = comprimento;
-let px = parseInt(Math.random() * x2 + 1);
-let py = parseInt(Math.random() * y2 + 1);
+let x0 = 0;
+let y0 = 0;
+let x1 = largura;
+let y1 = comprimento;
+
+console.log("Coordenadas (x0 y0 x1 y1):", x0, y0, x1, y1)
+
+let px = parseInt(Math.random() * x1 + 1);
+let py = parseInt(Math.random() * y1 + 1);
 
 while (px != 0 || py != 0) {
     let movimento = prompt(`[Pos: ${px},${py}] Indique o movimento (direção, passos - ex.: n,3)`);
